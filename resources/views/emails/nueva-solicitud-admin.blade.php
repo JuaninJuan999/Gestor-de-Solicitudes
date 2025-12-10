ff77f62ffebcd1e85369f5ef5d266f3973047a61
juan <kall.su999@gmail.com>
2025-12-09 21:26:47 -0500

    Avance proyecto

--- resources/views/emails/nueva-solicitud-admin.blade.php
+++ resources/views/emails/nueva-solicitud-admin.blade.php
@@ -6,8 +6,8 @@ Se ha registrado una nueva solicitud en el sistema.
 - Consecutivo: **{{ $solicitud->consecutivo ?? 'Sin consecutivo' }}**
 - Tipo: **{{ ucwords(str_replace('_', ' ', $solicitud->tipo_solicitud)) }}**
 - Estado: **{{ ucfirst($solicitud->estado) }}**
-- Usuario: **{{ $solicitud->user->name ?? 'N/A' }}**
-- Área solicitante: **{{ $solicitud->area_solicitante ?? 'N/A' }}**
+- Usuario: **{{ $solicitud->user->name ?? 'N/A' }} ({{ $solicitud->user->email ?? '' }})**
+- Área solicitante: **{{ $solicitud->user->area ?? 'Sin área asignada' }}**
 
 @component('mail::button', ['url' => route('solicitudes.show', $solicitud->id)])
 Ver solicitud en el sistema
